6844cb8fb46cff3531aff85daa7deb47880f31ca
CapitalLj <2438296284@qq.com>
2022-11-11 11:24:05 +0800

    master

---
+++ test/faops_splitabout.sh
@@ -0,0 +1,14 @@
+#!/usr/bin bash
+# splitting to chunks about specified size
+# usage:
+#     faops split-about [options] <in.fa> <approx_size> <outdir>
+
+# options:
+#     -e         sequences in one file should be EVEN
+#     -m INT     max parts
+
+faops split-about fasta.fa 400 ./output/splitabout01
+
+faops split-about -e fasta.fa 400 ./output/splitabout02
+
+faops split-about -m 5 fasta.fa 100 ./output/splitabout03
---
+++ test/faops_splitname.sh
@@ -0,0 +1,17 @@
+#!/wus/bin bash
+# splitting by sequence names
+# usage:
+#     faops split-name [options] <in.fa> <outdir>
+
+# options:
+#     -l INT     sequence line length [80]
+
+faops split-name fasta.fa ./output/splitname01
+
+
+
+
+
+
+
+
